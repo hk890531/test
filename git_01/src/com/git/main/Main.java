11bfbaa011c6564cb42eb8cce79ee7370f0b6ea4
mimi <mimi@mimi.com>
2021-02-03 17:16:10 +0900

    master : Main

--- git_01/src/com/git/main/Main.java
+++ git_01/src/com/git/main/Main.java
@@ -7,7 +7,8 @@ public class Main {
 		System.out.println("Hello HTML!");
 		System.out.println("Hello Servlet!");
 		System.out.println("Hello JSP!");
-		System.out.println("Hello Spring");
-		System.out.println("Hello jQuery");
+		System.out.println("Hello Spring!");
+		System.out.println("Hello jQuery!");
+		System.out.println("Hello Javascript!"); 
 	}
 }
